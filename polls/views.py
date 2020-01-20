9708fef110c3c331228d4b8c19072b55af7538fb
Hackerman272 <CoolPassword228>
2020-01-20 18:12:06 +0300

    0.11

--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,108 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+import json
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from django import forms
+
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.template import loader
+# from django.http import Http404
+
+from .models import Question, Choice
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    return HttpResponse('You are looking at question %s.' % question_id)
+@csrf_exempt
+def get_url(request):
+    data = json.loads(request.body)
+    text = data.get('text', None)
+    post = Question.objects.create(question_text=text)
+    post.save()
+    # json_string = request.POST.get()
+    # data = str(json.loads()[0])
+    # text = Question.objects.create(question_text=data)
+    # text.save()
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You did not select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+#
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+#
+#
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     # get_object_or_404()
+#     # get_list_or_404()
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#
+
+
+
+
+# from django.shortcuts import render
+#
+# from .models import Question
+#
+#
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 
 # def hyindex(request):
